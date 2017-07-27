dfb840bcc8a7f49ed98ba7de8883a0cef33e853a
DMonMac <dmmacaso@gmail.com>
2017-07-28 03:49:52 +0800

    Modified header and footer

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  get '/signup', to: 'users#new'
+
+  root "static_pages#home"
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
