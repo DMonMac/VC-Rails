02af3bb98a3ef5c4204fd41e1e3c2f98d96ed3e9
DMonMac <dmmacaso@gmail.com>
2017-07-28 10:58:33 +0800

    Adjusted look

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,11 @@ class StaticPagesController < ApplicationController
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
+
+  def home_logged_in
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   root "static_pages#home"
+  get '/home', to: "static_pages#home_logged_in"
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get '/microposts', to: 'static_pages#home_logged_in'
+
   resources :users do
     member do
       get :following, :followers
