cf345b94d61c5a244f0440ebbd9e6d89b813daae
DMonMac <dmmacaso@gmail.com>
2017-08-03 05:34:34 +0800

    completed sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id]) # This is similar to User.find(<user_id which was auto generated when creating a new user>)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) # Defined below and under 'private'
+    if @user.save
+      flash[:success] = "You have successfully signed in. Welcome, #{@user.first_name}!"
+      redirect_to @user # Handle successful save: Go to another page/path. Here the command equals 'redirect_to user_url(@user)'
+    else
+      render 'new'
+    end
+  end
+
+  private # Stuff under this are used internally by this controller and need not be exposed to external users via the web.
+    def user_params
+      params.require(:user).permit(:email,
+                                   :first_name,
+                                   :last_name,
+                                   :password,
+                                   :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
 
+  post '/signup', to: 'users#create'
+  resources :users
+  get '/signup', to: 'users#new'
   root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
