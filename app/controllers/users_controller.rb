09d9f9fb6dcb58a9aa82feca25efb44ae5670308
DMonMac <dmmacaso@gmail.com>
2017-08-04 11:03:06 +0800

    Basic authentication complete CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.all # This should list all users
@@ -39,6 +40,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
   private # Stuff under this are used internally by this controller and need not be exposed to external users via the web.
     def user_params
       params.require(:user).permit(:email,
@@ -62,6 +69,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms admin user
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
