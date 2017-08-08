25b189a7de5dbf35fc21041096d4babc766487bf
DMonMac <dmmacaso@gmail.com>
2017-08-08 19:51:32 +0800

    added first name and last name to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :homes
   root 'static_pages#home'
 
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
