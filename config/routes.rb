d7af67165ea73e1b8789ab76dbd5b83c811e4715
DMonMac <dmmacaso@gmail.com>
2017-08-13 18:06:29 +0800

    added user index and profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show]
+
   resources :homes
   root 'static_pages#home'
 
   devise_for :users, controllers: { registrations: 'users/registrations' },
+                     path: 'auth',
                      path_names: { sign_in: 'login', sign_out: 'logout' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
